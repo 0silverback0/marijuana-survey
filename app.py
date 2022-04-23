f99d7b3fefc346d2243e112531b2f263271659b8
marcell <fourmarz@yahoo.com>
2022-04-23 13:10:19 -0400

    to heroku

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ from psycopg2 import IntegrityError
 from models import connect_db, db, User
 from forms import UserForm
 from sqlalchemy import exc 
+import os
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///bud' # create and change data base
+app.config['SQLALCHEMY_DATABASE_URI'] =  os.environ.get('DATABASE_URL'
+'postgres:///flask-heroku') # 'postgresql:///bud' # create and change data base
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'shh')
 
 connect_db(app)
 db.create_all()
