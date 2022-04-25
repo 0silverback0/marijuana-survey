a2da70afc2bbb6e8a6ee5e1f5b28afb173e3a29e
marcell <fourmarz@yahoo.com>
2022-04-24 20:07:49 -0400

    added alert

---
+++ static/app.js
@@ -0,0 +1,11 @@
+alert(
+`This short survey was created to collect real data from actual people.
+
+ As far as we know there has not been any newly released data about
+ peoples smoking habits, and to be honest we don't trust any of the prior data collected.
+
+ We will not sell any of your information, and your email is only used 
+ to provide you with the results of our findings.
+ 
+ Please send this survey to anyone you know who is a user of the ganja!
+`)
